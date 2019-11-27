b5c9ea0f83d93d4573cbc6ba12bf81cb8d190326
Akshay Joshi <akshay.joshi.m@gmail.com>
2019-11-27 12:22:49 +0530

    added commands

--- jenkins_shell_test.sh
+++ jenkins_shell_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "******************* Executing jenkins_shell_test.sh *******************"
 ls -l
 echo "Current Date : `date`"
 systemctl status jenkins
 systemctl status ssh
+who am i
