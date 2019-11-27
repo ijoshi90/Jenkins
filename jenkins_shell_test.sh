2c7a201f4419604443a4f99009bd69be8396946a
Akshay Joshi <akshay.joshi.m@gmail.com>
2019-11-27 12:37:39 +0530

    added pwd to know jenkins temp directory

--- jenkins_shell_test.sh
+++ jenkins_shell_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "******************* Executing jenkins_shell_test.sh *******************"
+echo "Current Directory : `pwd`"
 ls -l
 echo "Current Date : `date`"
 systemctl status jenkins
