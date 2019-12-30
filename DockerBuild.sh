85e7032d0d25d616aeffd3c6b8a519cd7e8593d6
Akshay Joshi <akshay.joshi.m@gmail.com>
2019-12-30 18:33:59 +0100

    Update DockerBuild.sh

--- DockerBuild.sh
+++ DockerBuild.sh
@@ -1,8 +1,7 @@
 ls -lrt
 pwd
-sudo cp docker_build.sh /home/akshay/
+cp docker_build.sh /home/akshay/
 cd /home/akshay/
-sudo chown docker:docker docker_build.sh
 pwd
 sh docker_build.sh
 #rm -rf /home/akshay/docker_build.sh
